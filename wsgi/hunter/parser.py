037d7c0e89dfdb4cc7e41eac73698c8529e7a5a3
Vignesh Sarma <vignesh.sarma@gmail.com>
2012-04-02 01:09:18 +0530

    using HTMLParse to parse html

--- wsgi/hunter/parser.py
+++ wsgi/hunter/parser.py
@@ -1,26 +1,37 @@
 from HTMLParser import HTMLParser
 
+
 # create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-        self.data =""
+class LinkFinder(HTMLParser):
+    def start_parsing(self,content):
+        self.data = ""
         self.url = []
+        self.entered_title=False
+        self.title = ""
+        self.feed(content)
+        return self.url,self.data,self.title
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag, attrs
+        #print "Encountered a start tag:", tag, attrs
         if tag== 'a':
             for at in attrs:
                 if at[0]=="href":
-                    url.append(at[1])
+                    self.url.append(at[1])
+        elif tag == "title":
+            self.entered_title = True
     def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
+        #print "Encountered an end tag :", tag
+        if tag == "title":
+            self.entered_title=False
     def handle_data(self, data):
-        print "Encountered some data  :",
-        self.data += data
+        #print "Encountered some data  :",
+        if self.entered_title:
+            self.title = data
+            
+        self.data += " "+data.lower()
 
 
 
 if __name__=="__main__":
     # instantiate the parser and fed it some HTML
     parser = MyHTMLParser()
-    parser.feed('<html><head><title>Test</title></head>'
-            '<body id="bla"><h1>Parse me!</h1></body></html>')
+    print parser.start_parsing('<html><head><title>Test</title></head>'+ '<body id="bla"><a href="#" ><h1>Parse me!</h1></a></body></html>')
--- wsgi/hunter/search.py
+++ wsgi/hunter/search.py
@@ -1,4 +1,5 @@
-
+from cache import cache
+from parser import LinkFinder
 
 def qsort(tosort,ranks):
     if tosort == []: 
@@ -10,6 +11,7 @@ def qsort(tosort,ranks):
         return greater + [pivot] + lesser
 
 def ordered_search(index, ranks, keyword):
+    keyword=keyword.lower()
     if keyword in index:
         results = index[keyword]
         return qsort(results,ranks)
@@ -17,123 +19,6 @@ def ordered_search(index, ranks, keyword):
         
 
 
-cache = {
-   'http://udacity.com/cs101x/urank/index.html': """<html>
-<body>
-<h1>Dave's Cooking Algorithms</h1>
-<p>
-Here are my favorite recipies:
-<ul>
-<li> <a href="http://udacity.com/cs101x/urank/hummus.html">Hummus Recipe</a>
-<li> <a href="http://udacity.com/cs101x/urank/arsenic.html">World's Best Hummus</a>
-<li> <a href="http://udacity.com/cs101x/urank/kathleen.html">Kathleen's Hummus Recipe</a>
-</ul>
-
-For more expert opinions, check out the 
-<a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a> 
-and <a href="http://udacity.com/cs101x/urank/zinc.html">Zinc Chef</a>.
-</body>
-</html>
-
-
-
-
-
-
-""",
-   'http://udacity.com/cs101x/urank/zinc.html': """<html>
-<body>
-<h1>The Zinc Chef</h1>
-<p>
-I learned everything I know from 
-<a href="http://udacity.com/cs101x/urank/nickel.html">the Nickel Chef</a>.
-</p>
-<p>
-For great hummus, try 
-<a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
-
-</body>
-</html>
-
-
-
-
-
-
-""",
-   'http://udacity.com/cs101x/urank/nickel.html': """<html>
-<body>
-<h1>The Nickel Chef</h1>
-<p>
-This is the
-<a href="http://udacity.com/cs101x/urank/kathleen.html">
-best Hummus recipe!
-</a>
-
-</body>
-</html>
-
-
-
-
-
-
-""",
-   'http://udacity.com/cs101x/urank/kathleen.html': """<html>
-<body>
-<h1>
-Kathleen's Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Open a can of garbonzo beans.
-<li> Crush them in a blender.
-<li> Add 3 tablesppons of tahini sauce.
-<li> Squeeze in one lemon.
-<li> Add salt, pepper, and buttercream frosting to taste.
-</ol>
-
-</body>
-</html>
-
-""",
-   'http://udacity.com/cs101x/urank/arsenic.html': """<html>
-<body>
-<h1>
-The Arsenic Chef's World Famous Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>.
-<li> Force her to make hummus for you.
-</ol>
-
-</body>
-</html>
-
-""",
-   'http://udacity.com/cs101x/urank/hummus.html': """<html>
-<body>
-<h1>
-Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Go to the store and buy a container of hummus.
-<li> Open it.
-</ol>
-
-</body>
-</html>
-
-
-
-
-""",
-}
 
 def get_page(url):
     if url in cache:
@@ -162,7 +47,7 @@ def get_page(url):
 #             break
 #     return links
 
-def get_all_links(page):
+#def get_all_links(page):
     
 
     
@@ -197,8 +82,8 @@ def crawl_web(seed): # returns index, graph of inlinks
         page = tocrawl.pop()
         if page not in crawled:
             content = get_page(page)
+            outlinks,content,title = LinkFinder().start_parsing(content)
             add_page_to_index(index, page, content)
-            outlinks = get_all_links(content)
             graph[page] = outlinks
             union(tocrawl, outlinks)
             crawled.append(page)
@@ -236,22 +121,24 @@ def compute_ranks(graph):
 index, graph = crawl_web('http://udacity.com/cs101x/urank/index.html')
 ranks = compute_ranks(graph)
 
-#print ordered_search(index, ranks, 'Hummus')
+if __name__ == "__main__":
+    
+    print ordered_search(index, ranks, 'Hummus')
 #>>> ['http://udacity.com/cs101x/urank/kathleen.html', 
 #    'http://udacity.com/cs101x/urank/nickel.html', 
 #    'http://udacity.com/cs101x/urank/arsenic.html', 
 #    'http://udacity.com/cs101x/urank/hummus.html', 
 #    'http://udacity.com/cs101x/urank/index.html'] 
 
-#print ordered_search(index, ranks, 'the')
+    print ordered_search(index, ranks, 'the')
 #>>> ['http://udacity.com/cs101x/urank/nickel.html', 
 #    'http://udacity.com/cs101x/urank/arsenic.html', 
 #    'http://udacity.com/cs101x/urank/hummus.html', 
 #    'http://udacity.com/cs101x/urank/index.html']
 
 
-#print ordered_search(index, ranks, 'babaganoush')
+    print ordered_search(index, ranks, 'babaganoush')
 #>>> None
 
 
-print "loading search.py"
+    print "loading search.py"
