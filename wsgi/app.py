b986c2752f291959d81f3ff639e45c622723fb4d
Vignesh Sarma <vignesh.sarma@gmail.com>
2012-04-05 16:42:52 +0530

    app updated for the classyness

--- wsgi/app.py
+++ wsgi/app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 #url_for('static',filename='style.css')
 #url_for('static',filename='favicon.ico')
-
+hunt = hunter_search.WebHunter()
 @app.route('/')
 def hello():
     return render_template('base.html')#'Hello World,from Vignesh'+'!'*5
 
 @app.route('/web')
 def web():
-    results = hunter_search.ordered_search(hunter_search.index, hunter_search.ranks, request.args.get('q'))
+    results = hunt.ordered_search( request.args.get('q'))
     
     if not results:
         results = ["No results found!!!try something else."]
-    return render_template('searched.html',results=results,graph=hunter_search.graph)
+    return render_template('searched.html',results=results,graph=hunt.graph)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
